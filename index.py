e2cbd83f2489c36937739a65ef3c1eb70f4c6f2e
Ayush Rameja <ayushrwork@gmail.com>
2021-04-24 18:03:17 +0530

    More Optimized and Good Looking

--- index.py
+++ index.py
@@ -1,147 +1,167 @@
-# import
-import random 
+# Import
+import random
+import time
 import sys
+import os
 
-# main function
-def main():
-    conditionMain = True;
-    while conditionMain:
+# Loading Animation
+def load_animation():
+    load_str = "Loading Application..."
+    ls_len = len(load_str)
+    animation = "|/-\\"
+    anicount = 0
+    counttime = 0
+    i = 0
+    while counttime != 15:
+        time.sleep(0.075)
+        load_str_list = list(load_str)
+        x = ord(load_str_list[i])
+        y = 0
+        if x != 32 and x != 46:
+            if x > 90:
+                y = x - 32
+            else:
+                y = x + 32
+            load_str_list[i] = chr(y)
+        res = ""
+        for j in range(ls_len):
+            res = res + load_str_list[j]
+        sys.stdout.write("\r" + res + animation[anicount])
+        sys.stdout.flush()
+        load_str = res
+        anicount = (anicount + 1) % 4
+        i = (i + 1) % ls_len
+        counttime = counttime + 1
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+# Main Menu
+def mainMenu():
+    while True:
         # !conditions
-        print("Main Menu")
-        print("Please enter one of the number:")
-        print("1. Dice roll.")
-        print("2. Guessing the number game.")
-        print("3. Random password generator.")
-        print("4. Exit")
+        print("-------- MAIN MENU -------- \n")
+        print("Hi, What do you want me to do? :")
+        print("Enter 'A or a' Dice roll.")
+        print("Enter 'B or b' Guessing the number game.")
+        print("Enter 'C or c' Random password generator.")
+        print("Enter 'E or e' Exit")
+        print("--------------------------")
 
         # !choose
         print("\n")
-        try:
-            choose = input("Enter your choice: ")
-            if int(choose) == 1:
-                diceRoller()
-            elif int(choose) == 2:
-                guessingNumber()
-            elif int(choose) == 3:
-                randPassword()
-            elif int(choose) == 4:
-                print("Bye")
-                break
-            else:
-                print("Invalid Input.\n")
-                print("Running Again.\n")
-                continue
-        except:
-            print("Invalid Input.\n")
-            print("Running Again\n")
-            continue
-
-        
-# dice roller
-def diceRoller():
-    print("\n")
-    condition = True
-    while condition:     
-        try:
-            print("<-- ROLLING DICE -->")    
-            number = random.randint(1,6)         
-            print(f"Number is: {number}")
-            print(f"---------------")
-            print("1. Roll again")     
-            print("2. Exit to Main Menu")       
-            print("\n") 
-            user = input("Enter: ")    
-            if int(user)==1:
-                print("Rolling Again!")         
-                continue
-            elif int(user)==2: 
-                print("\n")
-                condition = False
-                break
-            else:
-                print("Invalid input.\n")
-                print("Running Again.\n")
-                continue
-        except:
-            print("Invalid Input.\n")
-            print("Running Again.\n")
-            continue
-# guessing the number game
-def guessingNumber():
-    print("\n")
-    while True:
-        print("This is a Random Number Guessing game.")
-        print("Range of numbers is from 1 to 10.")
-        number = random.randint(1,10)
-        user = input("Guess Number: ")
-
-        try:
-            if int(user) == number:
-                print("Hurray!!")
-                print(f"you guessed the number right it's {number}")
-
-            else:
-                if int(user) > 10 or int(user) < 1:
-                    print("Enter Within range")
-                elif int(user) != number and int(user) <= 10 and int(user) >= 1:
-                    print(f"You guessed wrong number.The number was {number}")
-        except:
-            print("Invalid Input.\n")
-    
-        print("\n")    
-        print("Press 'A' to play again")
-        print("Press 'M' to Exit to Main Menu")
-        cond = input("Enter your choice: ")
-        if cond == "A" or cond == "a":
-            continue     
-        elif cond == "M" or cond == "m":
-            break
+        choose = input("Enter your choice: ")
+        if choose == "A" or choose == "a":
+            load_animation()
+            diceRoller()
+        elif choose == "b" or choose == "B":
+            load_animation()
+            guessingNumber()
+        elif choose == "C" or choose == "c":
+            load_animation()
+            randPassword()
+        elif choose == "E" or choose == "e":
+            print("Quitting")
+            sys.exit()
         else:
-            print("Invalid input guessing")
-            print("Going to Main Menu")
-            break
+            print("Enter a Valid Input!")
+            print("Running Again!")
+            mainMenu()
 
 
-# random password
+# Dice Roller
+def diceRoller():
+     print("\n")
+     print("<-- ROLLING DICE -->")
+     number = random.randint(1, 6)
+     print(f"Number is: {number}")
+     print(f"---------------")
+     Recall(1)
+
+
+# Guessing The Number Game
+def guessingNumber():
+     print("\n")
+     print("This is a Random Number Guessing game.")
+     print("Range of numbers is from 1 to 10.")
+     number = random.randint(1, 10)
+     user = input("Guess Number: ")
+
+     try:
+          if int(user) == number:
+               print("Hurray!!")
+               print(f"you guessed the number right it's {number}")
+
+          else:
+               if int(user) > 10 or int(user) < 1:
+                    print("Enter Within range")
+               elif int(user) != number and int(user) <= 10 and int(
+                    user) >= 1:
+                    print(f"You guessed wrong number.The number was {number}")
+     except:
+          print("Invalid Input.\n")
+
+     Recall(2)
+
+
+# Random Password
 def randPassword():
-     while True:
-          try:
-               print("Random Password Generator:")
-               passlen = int(input("Enter the length of password: "))
-               s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-               if passlen >= 1 and passlen < 30:
-                    p =  "".join(random.sample(s,passlen))
-                    print (p)
-               else:
-                    print("please enter length between (1 - 30)")
-                    print("Running again.....")
-                    continue
-     
-          except:
-               print("Invalid Input!")
-               print("Running Again..... \n")
-               continue
+     try:
+          print("Random Password Generator:")
+          passlen = int(input("Enter the length of password: "))
+          s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+          if passlen >= 1 and passlen < 30:
+               p = "".join(random.sample(s, passlen))
+               print(p)
+          else:
+               print("please enter length between (1 - 30)")
+               print("Running again.....")
+
+     except:
+          print("Invalid Input!")
+          print("Running Again..... \n")
+
+     Recall(3)
+
+# Recall
+def Recall(flagTemp):
+     print("\n")
+     print("-----------------------------------")
+     print("Enter 'A' or 'a' to run again")
+     print("Enter 'M' or 'm' for Main menu")
+     print("Enter 'E' or 'e' to Exit")
+     print("-----------------------------------")
+     dis = input("Enter :")
+     if dis == "A" or dis == "a":
+          reCallChoice(flagTemp)
+     elif dis == "M" or dis == "m":
+          print("going home!")
+          mainMenu()
+     elif dis == "E" or dis == "e":
+          print("Bye!")
+          sys.exit()    
+     else:
+        print("Invalid Input!")
+        print("Running again!")
+        Recall()
+
+# Flag Calling Function
+def reCallChoice(flag):
+     if flag == 1:
+          diceRoller()
+     elif flag == 2:
+          guessingNumber()
+     elif flag == 3:
+          randPassword()
+
+# main function
+def main():
+    mainMenu()
 
 
-          print("\n")
-          print("1. Run Again.")
-          print("2. Exit to Main Menu.")
-          choice = input("Choice: ")
-          try:
-               if int(choice) == 1:
-                    continue
-               elif int(choice) == 2:
-                    break
-               else:
-                    print("Invalid Input.")
-                    print("Running Again!")
-                    continue
-          except:
-               print("Invalid input.")
-               print("Running again")
-               continue
-        
-        
-
+# calling
 if __name__ == "__main__":
+    load_animation()
     main()
